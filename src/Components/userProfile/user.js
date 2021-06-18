b6c0bb3f60e41bfd2eb0485ad0aeeabb0a791b2b
dev-nasreen <nasreen.akter34@gmail.com>
2021-06-18 20:52:36 +0600

    simly completed

--- src/Components/userProfile/user.js
+++ src/Components/userProfile/user.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import listItems from '../fakeData/listItems.json'
-import unpinned from '../fakeData/unpinnedCard.json'
 import pinned from '../fakeData/pinnedCard.json'
 import AvatarMaker from '../../images/AvatarMaker.png'
 import { FaFacebook } from 'react-icons/fa';
@@ -67,9 +66,7 @@ const User = () => {
        setItems(listItems)
     }, []);
     
-    // useEffect(() => {
-    //   setUnpinnedCard(unpinned)
-    // }, []);
+   
     
     useEffect(() => {
       setPinnedCard(pinned)
@@ -151,33 +148,7 @@ const User = () => {
     </ul>
       </div>
       </section>
-      {/* <section className="unpinned-card-area">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-     {
-        unpinnedCards.map(unpinitem =>(
-    <li x-for="item in items">
-      <a href="item.url" className="shadow-md hover:bg-light-blue-500 hover:border-transparent hover:shadow-lg group block rounded-lg p-4 border border-gray-200 bg-white">
-        <dl className="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-start h-40">
-          <div className="">
-            <dt className="sr-only">Title</dt>
-            <dd className="text-left group-hover:text-pink leading-6 font-medium text-pink-600">
-            {unpinitem.title}
-            </dd>
-          </div>
-          <div>
-            <dt className="sr-only">Category</dt>
-            <dd className="text-left group-hover:text-light-blue-200 text-sm font-medium text-lg sm:mb-4 lg:mb-0 xl:mb-4">
-              {unpinitem.desc}
-            </dd>
-          </div>
-       
-        </dl>
-      </a>
-    </li>
-       ))
-    } 
-    </ul>
-      </section> */}
+
     <section className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4">
      <div >
      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
